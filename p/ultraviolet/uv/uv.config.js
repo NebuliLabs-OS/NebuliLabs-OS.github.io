4af90549a2716ab7538e796367aa64951ca077c6
NebuliLabs-OS <n3bulios@gmail.com>
2025-09-08 12:13:41 -0500

    Update uv.config.js

--- p/ultraviolet/uv/uv.config.js
+++ p/ultraviolet/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-	prefix: "/NebuliOS/p/ultraviolet/ultraviolet/",
+	prefix: "/p/ultraviolet/ultraviolet/",
 	bare: "https://meximathurbanned.global.ssl.fastly.net/tspmo/",
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
-	handler: "/NebuliOS/p/ultraviolet/uv/uv.handler.js",
-	bundle: "/NebuliOS/p/ultraviolet/uv/uv.bundle.js",
-	config: "/NebuliOS/p/ultraviolet/uv/uv.config.js",
-	sw: "/NebuliOS/p/ultraviolet/uv/uv.sw.js",
+	handler: "/p/ultraviolet/uv/uv.handler.js",
+	bundle: "/p/ultraviolet/uv/uv.bundle.js",
+	config: "/p/ultraviolet/uv/uv.config.js",
+	sw: "/p/ultraviolet/uv/uv.sw.js",
 };
