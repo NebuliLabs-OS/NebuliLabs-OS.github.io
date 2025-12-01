88b42e96bce33ed7b90390c9ad7b7e691a843049
Aquanix <68292357+NebuliLabs@users.noreply.github.com>
2025-12-01 16:57:25 -0600

    Delete p/ultraviolet directory

--- p/ultraviolet/uv/uv.config.js
+++
@@ -1,9 +0,0 @@
-self.__uv$config = {
-	prefix: "/p/ultraviolet/ultraviolet/",
-	encodeUrl: Ultraviolet.codec.xor.encode,
-	decodeUrl: Ultraviolet.codec.xor.decode,
-	handler: "/p/ultraviolet/uv/uv.handler.js",
-	bundle: "/p/ultraviolet/uv/uv.bundle.js",
-	config: "/p/ultraviolet/uv/uv.config.js",
-	sw: "/p/ultraviolet/uv/uv.sw.js",
-};
